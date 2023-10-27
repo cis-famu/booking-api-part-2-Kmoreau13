f7ac5160480c2c5860b679e901d0434d5a17f77e
Keisha Moreau <keisha1.moreau@famu.edu>
2023-10-27 18:39:42 -0400

    Update ReservationService.java

--- src/main/java/edu/famu/hotelprodject/Service/ReservationService.java
+++ src/main/java/edu/famu/hotelprodject/Service/ReservationService.java
@@ -28,43 +28,17 @@ public class ReservationService {
 
 
 
-            // Retrieve passenger details
+            /* Retrieve User details
             DocumentReference userRef = (DocumentReference) document.get("customer");
             if (userRef != null) {
                 DocumentSnapshot passengerSnapshot = userRef.get().get();
                 if (passengerSnapshot.exists()) {
                     UserService service = new UserService();
-                    User user = UserService.documentSnapshotToUser(UserSnapshot);
-                    reservation.setCustomer(User);
+                   user = UserService.documentSnapshotToUser(UserSnapshot);
+                    //reservation.setCustomer(User);
                 }
             }
-
-            // Retrieve flight details
-            DocumentReference flightRef = (DocumentReference) document.get("flightNumber");
-            if (flightRef != null) {
-                DocumentSnapshot flightSnapshot = flightRef.get().get();
-                if (flightSnapshot.exists()) {
-                    FlightService service = new FlightService();
-                    Flight flight = service.documentSnapshotToFlight(flightSnapshot);
-                    booking.setFlightNumber(flight);
-                }
-            }
-
-            // Retrieve aircraft details
-            DocumentReference paymentRef = (DocumentReference) document.get("paymentID");
-            if (paymentRef != null) {
-                DocumentSnapshot paymentSnapshot = paymentRef.get().get();
-                if (paymentSnapshot.exists()) {
-                    PaymentService service = new PaymentService();
-                    Payment payment = service.documentSnapshotToPayment(paymentSnapshot);
-                    booking.setPaymentID(payment);
-                }
-            }
-
-            return booking;
-        }
-        return null;
-    }
+*/
 
 
 }
